f47225c258dc5a1c1bb99348909f2a67d17658ab
909ma <sin00021@naver.com>
2023-04-26 16:24:12 +0900

    :anger:

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 let currentQuestionIndex = 1;
 const questionContainer = document.querySelector(".question-container");
-const questions = questionContainer.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question");
+const answerBtn = document.querySelector(".show");
+const answer = document.querySelector(".answer");
 
+//Next 기능
 function nextQuestion() {
   questions[currentQuestionIndex - 1].classList.remove("active");
   currentQuestionIndex =
     currentQuestionIndex === questions.length ? 1 : currentQuestionIndex + 1;
   questions[currentQuestionIndex - 1].classList.add("active");
 }
+
+//Back 기능
 function backQuestion() {
   questions[currentQuestionIndex - 1].classList.remove("active");
   currentQuestionIndex =
@@ -15,12 +20,15 @@ function backQuestion() {
   questions[currentQuestionIndex - 1].classList.add("active");
 }
 
+//Go to 기능
 function goToQuestion() {
+  // 이전에 선택된 문제를 지웁니다.
+  //questions.forEach((question) => question.classList.remove("active"));
   questions[currentQuestionIndex - 1].classList.remove("active");
 
   // 입력된 문제 번호를 가져옵니다.
   const questionNum = document.getElementById("questionNumInput").value;
-
+  currentQuestionIndex = questionNum - 1;
   // 해당 문제 번호에 해당하는 문제 ID를 가져옵니다.
   const questionId = `question${questionNum}`;
 
@@ -30,7 +38,37 @@ function goToQuestion() {
     alert(`Question ${questionNum} does not exist!`);
     return;
   }
-
-  // 해당 문제 위치로 스크롤합니다.
+  // 해당 문제 위치로 점프합니다.
   questions[questionNum - 1].classList.add("active");
 }
+
+// 엔터키를 눌렀을 때 go to 버튼 클릭
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault(); // 기본 이벤트를 취소하여 페이지 이동 방지
+    document.getElementById("goToQuestion").click(); // go to 버튼 클릭
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.keyCode === 37) {
+    // 왼쪽 화살표
+    backQuestion();
+  } else if (event.keyCode === 39) {
+    // 오른쪽 화살표
+    nextQuestion();
+  }
+});
+
+//해답 보이기, 가리기 기능
+answerBtn.onclick = function () {
+  if (answer.style.display === "none") {
+    answer.style.display = "block";
+    //answer.forEach((answer) => (answer.style.display = "block"));
+    answerBtn.textContent = "Hide";
+  } else {
+    answer.style.display = "none";
+    //answer.forEach((answer) => (answer.style.display = "none"));
+    answerBtn.textContent = "Show";
+  }
+};
